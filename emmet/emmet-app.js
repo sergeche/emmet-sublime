5c7d8a37408cd1b5df53badcdcd964410c19813d
Sergey Chikuyonok <serge.che@gmail.com>
2013-12-16 00:28:18 +0400

    Minor fixed in Select Item action

--- emmet/emmet-app.js
+++ emmet/emmet-app.js
@@ -7895,6 +7895,8 @@ define('utils/common',['require','exports','module','lodash','../assets/range'],
 	};
 });
 
+define('fs',{});
+define('path',{});
 /**
  * Module for working with file. Shall implement
  * IEmmetFile interface.
@@ -7906,6 +7908,8 @@ define('utils/common',['require','exports','module','lodash','../assets/range'],
  * require('./plugin/file')({
  * 	read: function() {...}
  * })
+ *
+ * By default, this module provides Node.JS implementation
  */
 
 if (typeof module === 'object' && typeof define !== 'function') {
@@ -7914,13 +7918,189 @@ if (typeof module === 'object' && typeof define !== 'function') {
 	};
 }
 
-define('plugin/file',['require','exports','module','lodash'],function(require, exports, module) {
+define('plugin/file',['require','exports','module','lodash','fs','fs','path','fs','path','fs'],function(require, exports, module) {
 	var _ = require('lodash');
-	return function fn(obj) {
+
+	// module is a function that can extend itself
+	module.exports = function(obj) {
 		if (obj) {
-			_.extend(fn, obj);
+			_.extend(module.exports, obj);
 		}
 	};
+
+	function bts(bytes) {
+		return _.map(bytes, function(b) {
+			return String.fromCharCode(b);
+		}).join('');
+	}
+
+	function isURL(path) {
+		var re = /^https?:\/\//;
+		return re.test(path);
+	}
+
+	return _.extend(module.exports, {
+		_parseParams: function(args) {
+			var params = {
+				path: args[0],
+				size: 0
+			};
+
+			args = _.rest(args);
+			params.callback = _.last(args);
+			args = _.initial(args);
+			if (args.length) {
+				params.size = args[0];
+			}
+
+			return params;
+		},
+
+		_read: function(params, callback) {
+			var fs = require('fs');
+			if (isURL(params.path)) {
+				var req = require(/^https:/.test(params.path) ? 'https' : 'http').get(params.path, function(res) {
+					var bufs = [];
+					var totalLength = 0;
+					var finished = false;
+					res
+						.on('data', function(chunk) {
+							totalLength += chunk.length;
+							bufs.push(chunk);
+							if (params.size && totalLength >= params.size) {
+								finished = true;
+								callback(null, Buffer.concat(bufs));
+								req.abort();
+							}
+						})
+						.on('end', function() {
+							if (!finished) {
+								finished = true;
+								callback(null, Buffer.concat(bufs));
+							}
+						});
+				}).on('error', callback);
+			} else {
+				if (params.size) {
+					var fd = fs.openSync(params.path, 'r');
+					var buf = new Buffer(params.size);
+					fs.read(fd, buf, 0, params.size, null, function(err, bytesRead) {
+						callback(err, buf)
+					});
+				} else {
+					callback(null, fs.readFileSync(params.path));
+				}
+			}
+		},
+
+		/**
+		 * Reads binary file content and return it
+		 * @param {String} path File's relative or absolute path
+		 * @return {String}
+		 */
+		read: function(path, size, callback) {
+			var params = this._parseParams(arguments);
+			this._read(params, function(err, buf) {
+				params.callback(err, err ? '' : bts(buf));
+			});
+		},
+
+		/**
+		 * Read file content and return it
+		 * @param {String} path File's relative or absolute path
+		 * @return {String}
+		 */
+		readText: function(path, size, callback) {
+			var params = this._parseParams(arguments);
+			this._read(params, function(err, buf) {
+				params.callback(err, err ? '' : buf.toString());
+			});
+		},
+		
+		/**
+		 * Locate <code>file_name</code> file that relates to <code>editor_file</code>.
+		 * File name may be absolute or relative path
+		 * 
+		 * <b>Dealing with absolute path.</b>
+		 * Many modern editors have a "project" support as information unit, but you
+		 * should not rely on project path to find file with absolute path. First,
+		 * it requires user to create a project before using this method (and this 
+		 * is not very convenient). Second, project path doesn't always points to
+		 * to website's document root folder: it may point, for example, to an 
+		 * upper folder which contains server-side scripts.
+		 * 
+		 * For better result, you should use the following algorithm in locating
+		 * absolute resources:
+		 * 1) Get parent folder for <code>editorFile</code> as a start point
+		 * 2) Append required <code>fileName</code> to start point and test if
+		 * file exists
+		 * 3) If it doesn't exists, move start point one level up (to parent folder)
+		 * and repeat step 2.
+		 * 
+		 * @param {String} editorFile
+		 * @param {String} fileName
+		 * @return {String} Returns null if <code>fileName</code> cannot be located
+		 */
+		locateFile: function(editorFile, fileName) {
+			var fs = require('fs');
+			var path = require('path');
+
+			if (isURL(fileName)) {
+				return fileName;
+			}
+
+			var dirname = editorFile, f;
+			fileName = fileName.replace(/^\/+/, '');
+			while (dirname && dirname !== path.dirname(dirname)) {
+				dirname = path.dirname(dirname);
+				f = path.join(dirname, fileName);
+				if (fs.existsSync(f))
+					return f;
+			}
+			
+			return '';
+		},
+		
+		/**
+		 * Creates absolute path by concatenating <code>parent</code> and <code>fileName</code>.
+		 * If <code>parent</code> points to file, its parent directory is used
+		 * @param {String} parent
+		 * @param {String} fileName
+		 * @return {String}
+		 */
+		createPath: function(parent, fileName, callback) {
+			var fs = require('fs');
+			var path = require('path');
+
+			var stat = fs.statSync(parent);
+			if (stat && !stat.isDirectory()) {
+				parent = path.dirname(parent);
+			}
+			
+			return callback(path.resolve(parent, fileName));
+		},
+		
+		/**
+		 * Saves <code>content</code> as <code>file</code>
+		 * @param {String} file File's absolute path
+		 * @param {String} content File content
+		 */
+		save: function(file, content) {
+			var fs = require('fs');
+			fs.writeFileSync(file, content, 'ascii');
+		},
+		
+		/**
+		 * Returns file extension in lower case
+		 * @param {String} file
+		 * @return {String}
+		 */
+		getExt: function(file) {
+			var m = (file || '').match(/\.([\w\-]+)$/);
+			return m ? m[1].toLowerCase() : '';
+		}
+	
+	});
 });
 /**
  * @author Sergey Chikuyonok (serge.che@gmail.com)
@@ -8622,8 +8802,6 @@ define('vendor/stringScore',['require','exports','module'],function(require, exp
 		}
 	};
 });
-define('fs',{});
-define('path',{});
 /**
  * Parsed resources (snippets, abbreviations, variables, etc.) for Emmet.
  * Contains convenient method to get access for snippets with respect of 
@@ -20175,8 +20353,7 @@ define('action/selectItem',['require','exports','module','lodash','../assets/ran
 		if (isBackward) {
 			ranges.reverse();
 		}
-		// console.log(ranges);
-
+		
 		// return next to selected range, if possible
 		var r = _.find(ranges, function(item) {
 			return item.equal(selRange);
@@ -20198,9 +20375,16 @@ define('action/selectItem',['require','exports','module','lodash','../assets/ran
 		}
 
 		// return range next to caret
-		return _.find(ranges, function(item) {
+		r = _.find(ranges, function(item) {
 			return item.end > selRange.start;
 		});
+
+		if (!r) {
+			// can’t find anything, just pick first one
+			r = ranges[0];
+		}
+
+		return r;
 	}
 	
 	function findNextCSSItem(editor) {
